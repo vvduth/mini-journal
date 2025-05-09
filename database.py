8b81d58cd19c60b3873ade991821418d6ffd3fc1
vvduth <ducthai060501@gmail.com>
2025-05-09 11:31:56 +0300

    Improve database query handling by setting row factory and returning cursor for entry retrieval

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 connection = sqlite3.connect("data.db")
+connection.row_factory = sqlite3.Row
 
 def create_table():
     with connection:
@@ -17,5 +18,6 @@ def add_entry(entry_content, entry_date):
 
 def get_entries():
     cursor = connection.cursor()
-    entries =  cursor.execute("SELECT * FROM entries")
-    return  entries
+    cursor.execute("SELECT * FROM entries")
+
+    return  cursor
