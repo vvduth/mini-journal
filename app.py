2f5146be6a757bab4c215ccafd3733db224adfea
vvduth <ducthai060501@gmail.com>
2025-05-09 11:02:47 +0300

    Add journal application with entry management and database integration

---
+++ app.py
@@ -0,0 +1,38 @@
+from database import add_entry, get_entries
+
+menu = """
+Please select one of the follwowing options:
+1) Add new entry
+2) View all entries
+3) Exit
+
+Your selection: """
+
+welcome_message = "Welcome to the journal app!"
+
+def prompt_new_entry():
+    entry_content = input("What have you learned to day? ")
+    entry_date = input("What is the date today? ")
+    add_entry(entry_content, entry_date)
+
+def view_entry(entries_list):
+    for entry in entries_list:
+        print(f"Date: {entry['date']}")
+        print(f"Content: {entry['content']}")
+        print("-" * 20)
+
+
+user_input = input(menu)
+
+while user_input != "3":
+    # for later use
+    if user_input == "1":
+        prompt_new_entry()
+    elif user_input == "2":
+        entries = get_entries()
+        view_entry(entries)
+    else:
+        print("Invalid selection. Please try again.")
+
+    user_input = input(menu)
+
---
+++ database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, content TEXT, date TEXT)")
+
+
+def add_entry(entry_content, entry_date):
+    entries.append({"content": entry_content, "date": entry_date})
+
+
+def get_entries():
+    return  entries
